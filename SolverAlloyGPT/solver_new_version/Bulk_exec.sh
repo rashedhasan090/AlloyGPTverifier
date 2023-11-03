4fa3b2234c07de5a0d5cf84e05eb8e78752ce4bf
Mohannadcse <mohannad.alhanahnah@gmail.com>
2023-11-02 21:17:04 -0500

    keep only the GPTEvaluator

--- SolverAlloyGPT/solver_new_version/Bulk_exec.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Loop through all .als files in the current directory
-for file in *.als
-do
-    # Run solver.jar with the current file as input
-    java -jar solver.jar $file
-done
